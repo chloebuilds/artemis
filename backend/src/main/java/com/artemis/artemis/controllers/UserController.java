b69ce5b8f9fc1b923033fe30904fc5eefc2ba8e4
Chloe Brown <chloemail@protonmail.com>
2021-11-03 11:14:00 +0000

    user service done

--- backend/src/main/java/com/artemis/artemis/controllers/UserController.java
+++ backend/src/main/java/com/artemis/artemis/controllers/UserController.java
@@ -1,4 +1,20 @@
 package com.artemis.artemis.controllers;
 
+import com.artemis.artemis.models.User;
+import com.artemis.artemis.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // FIND ALL
+    @GetMapping("/users")
+    public Iterable<User> getUsers() { return userService.findAllUsers(); }
 }
--- backend/src/main/java/com/artemis/artemis/services/UserService.java
+++ backend/src/main/java/com/artemis/artemis/services/UserService.java
@@ -1,4 +1,51 @@
 package com.artemis.artemis.services;
 
+import com.artemis.artemis.models.User;
+import com.artemis.artemis.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    public Iterable<User> findAllUsers() { return userRepo.findAll(); }
+
+    public User createUser(User user) { return userRepo.save(user); }
+
+    public Optional<User> findUser(Integer userId) {
+        Optional<User> optUser = userRepo.findById(userId);
+        if (optUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return optUser;
+    }
+
+    public void deleteUser(Integer userId) {
+        Optional<User> optUser = userRepo.findById(userId);
+        if (optUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        userRepo.deleteById(userId);
+    }
+
+    public User updateUser(Integer userId, User user) {
+        Optional<User> optUser = userRepo.findById(userId);
+        if (optUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        User userToUpdate = optUser.get();
+        userToUpdate.setUsername(user.getUsername());
+        userToUpdate.setEmail(user.getEmail());
+        userToUpdate.setPassword(user.getPassword());
+        userToUpdate.setLinkedin(user.getLinkedin());
+        userToUpdate.setGithub(user.getGithub());
+        userToUpdate.setPortfolio(userToUpdate.getPortfolio());
+        return userRepo.save(userToUpdate);
+    }
 }
